6991affef21979defbf7fc564a9bbe7fa810b368
n3s7or <nrmorales.93@gmail.com>
2021-12-06 22:09:45 -0500

    First valid request call

--- prices.py
+++ prices.py
@@ -1,10 +1,18 @@
+import os
+import requests
+
+BASE_URL = 'https://pro-api.coinmarketcap.com'
+API_KEY = os.environ.get('HX_CMC_APIKEY')
 
 
-API_KEY=''
+def _call(endpoint: str) -> dict:
+    headers = {'X-CMC_PRO_API_KEY': API_KEY}
+    url = BASE_URL + endpoint
+    return requests.get(url, headers=headers).json()
 
 
 def main():
-    pass
+    print(_call('/v1/cryptocurrency/map'))
 
 
 if __name__ == "__main__":
